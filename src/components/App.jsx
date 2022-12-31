1303fcfddcfa98940113a81382cfc272cfae7b62
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-01-01 00:32:52 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
 import { GlobalStyle } from './GlobalStyle';
 import FeedbackOptions from './FeedbackOptions';
@@ -66,58 +66,3 @@ export function App() {
     </div>
   );
 }
-
-// export class App1 extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleIncrement = name => {
-//     this.setState(prevState => ({ [name]: prevState[name] + 1 }));
-//   };
-
-//   countTotalFeedback = () => {
-//     const { good, neutral, bad } = this.state;
-//     return good + neutral + bad;
-//   };
-//   countPositiveFeedbackPercentage = () => {
-//     const { good, neutral, bad } = this.state;
-//     return good === 0 ? 0 : Math.round((100 * good) / (good + neutral + bad));
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-
-//     const total = this.countTotalFeedback();
-//     const positivePercentage = this.countPositiveFeedbackPercentage();
-
-//     return (
-//       <div>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={this.state}
-//             onFeedback={this.handleIncrement}
-//           />
-//         </Section>
-
-//         <Section title="Statistics">
-//           {total === 0 ? (
-//             <p> There is no feedback</p>
-//           ) : (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={total}
-//               positivePercentage={positivePercentage}
-//             />
-//           )}
-//         </Section>
-
-//         <GlobalStyle />
-//       </div>
-//     );
-//   }
-// }
